518dbe44d1d9987fc949529592ecf0894dbde33b
James Offer <james.offer@casa.gov.au>
2020-05-21 09:54:06 +1000

    updates

---
+++ va.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+
+  $('.footer').prepend('<div class="banner"><div class="container"><div class="row"><div class="col-xs-12">We are always looking for ways to improve our services. <a href="#">Provide feedback on myCASA</a></div></div></div></div>');
+
+});
